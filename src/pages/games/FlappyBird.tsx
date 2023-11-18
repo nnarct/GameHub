a14a93509221e1c931a1618381df44217c65eb5b
nnarct <nannapat1551@gmail.com>
2023-11-19 03:47:19 +0700

    style: add padding

--- src/pages/games/FlappyBird.tsx
+++ src/pages/games/FlappyBird.tsx
@@ -9,11 +9,14 @@ const FlappyBird = () => {
   });
 
   return (
-    <div className="bg-[#1a1a1a] min-h-screen flex flex-col items-center justify-center">
+    <div className="bg-[#1a1a1a] min-h-screen flex flex-col items-center justify-center pb-8">
       <Unity
         unityProvider={unityProvider}
-        style={{ borderRadius: "16px", width: "1280px", height: "720px" }}
-
+        style={{
+          borderRadius: "16px",
+          width: "1280px",
+          height: "720px",
+        }}
       />
     </div>
   );
