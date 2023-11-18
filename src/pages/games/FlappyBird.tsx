b3509d8f421001bd64c60fcfe951b95f420074dd
PARIIXZ <Woonchat@gmail.com>
2023-11-18 22:55:31 +0700

    feat: new page for flappy bird game

---
+++ src/pages/games/FlappyBird.tsx
@@ -0,0 +1,22 @@
+import { Unity, useUnityContext } from "react-unity-webgl";
+
+const FlappyBird = () => {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "flappyBird/Build/flappyBird.loader.js",
+    dataUrl: "flappyBird/Build/flappyBird.data",
+    frameworkUrl: "flappyBird/Build/flappyBird.framework.js",
+    codeUrl: "flappyBird/Build/flappyBird.wasm",
+  });
+
+  return (
+    <div className="bg-[#1a1a1a] min-h-screen flex flex-col items-center justify-center">
+      <Unity
+        unityProvider={unityProvider}
+        style={{ borderRadius: "16px", width: "1280px", height: "720px" }}
+
+      />
+    </div>
+  );
+};
+
+export default FlappyBird;
