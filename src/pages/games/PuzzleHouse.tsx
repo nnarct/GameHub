47dd8b71505e12e2c95b3869f1d71ce02d85fd06
nnarct <nannapat1551@gmail.com>
2023-12-10 14:06:53 +0700

    style: remove unused texts

--- src/pages/games/PuzzleHouse.tsx
+++ src/pages/games/PuzzleHouse.tsx
@@ -10,8 +10,6 @@ const PuzzleHouse = () => {
 
   return (
     <div className="bg-[#1a1a1a] min-h-screen flex flex-col items-center justify-center pb-8">
-      <p className="p-5 m-1">Server Number: {}</p>
-      <p className="p-5 m-1">Player Name: {}</p>
       <Unity
         unityProvider={unityProvider}
         style={{
