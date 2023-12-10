b003d7d5edfc70322e733b6c4e532ea86000f276
nnarct <nannapat1551@gmail.com>
2023-12-10 14:16:34 +0700

    feat: remove advertise from layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import Advertise from "./components/Advertise";
 import { Outlet } from "react-router-dom";
 
 const Layout: React.FC = () => {
   return (
     <>
       <Navbar />
-      <Advertise />
       <div className="min-h-[68px]" />
       <Outlet />
     </>
