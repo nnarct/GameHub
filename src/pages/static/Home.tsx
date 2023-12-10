b6fc111b2e2e0a8c51c27c5d469c6d6b7ef564c8
nnarct <nannapat1551@gmail.com>
2023-12-10 14:05:23 +0700

    style: remove dummy game & change game order

--- src/pages/static/Home.tsx
+++ src/pages/static/Home.tsx
@@ -5,50 +5,29 @@ import Wave from "../../components/Wave";
 import GameLogo from "../../components/GameLogo";
 import PuzzleHouseLogo from "../../assets/gameLogo/puzzle-house-logo.png";
 import FlappyBirdLogo from "../../assets/gameLogo/flappy-bird-logo.png";
-import BrickBreakerLogo from "../../assets/gameLogo/brick-breaker-logo.png"
+import BrickBreakerLogo from "../../assets/gameLogo/brick-breaker-logo.png";
 
 const Home: React.FC = () => {
   return (
     <>
-      <div className="min-h-screen bg-[#1a1a1a]">
+      <div className="min-h-screen bg-slate-800">
         <Hero />
         <Wave />
         <div className="container mx-auto py-8 px-4 gap-16 flex justify-center flex-wrap">
-          <GameLogo
-            game="Puzzle House"
-            img={PuzzleHouseLogo}
-            url="/puzzle-house"
-          />
           <GameLogo
             game="Flappy Bird"
             img={FlappyBirdLogo}
             url="/flappy-bird"
           />
           <GameLogo
+            game="Puzzle House"
+            img={PuzzleHouseLogo}
+            url="/puzzle-house"
+          />
+          <GameLogo
             game="Brick Breaker"
             img={BrickBreakerLogo}
             url="/brick-breaker"
-          />
-
-          <GameLogo
-            game="Puzzle House"
-            img={PuzzleHouseLogo}
-            url="/puzzle-house"
-          />
-          <GameLogo
-            game="Puzzle House"
-            img={PuzzleHouseLogo}
-            url="/puzzle-house"
-          />
-          <GameLogo
-            game="Puzzle House"
-            img={PuzzleHouseLogo}
-            url="/puzzle-house"
-          />
-          <GameLogo
-            game="Puzzle House"
-            img={PuzzleHouseLogo}
-            url="/puzzle-house"
           />
         </div>
       </div>
